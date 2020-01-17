ca0fbd420d33c471f7e5c724aaa6bd2b0391bd59
Martijn de Vos <mdmartijndevos@gmail.com>
2020-01-17 16:44:55 +0100

    Fixed IPv8 import
    Not adding the Anydex IPv8 version to the paths if it can already be imported, as is usually the case when running Tribler.

--- anydex/__init__.py
+++ anydex/__init__.py
@@ -1,6 +1,9 @@
 import os
 import sys
 
-# Make sure IPv8 can be imported
-dir_path = os.path.dirname(os.path.realpath(__file__))
-sys.path.insert(0, os.path.join(dir_path, "..", "pyipv8"))
+try:
+    import ipv8
+except ImportError:
+    # Make sure IPv8 can be imported
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    sys.path.insert(0, os.path.join(dir_path, "..", "pyipv8"))
