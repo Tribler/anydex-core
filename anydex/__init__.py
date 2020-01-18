883258394b68efcceb26af9b1ed0bbb07c0366d0
Martijn de Vos <mdmartijndevos@gmail.com>
2020-01-18 15:55:46 +0100

    Removed IPv8 submodule

--- anydex/__init__.py
+++ anydex/__init__.py
@@ -1,9 +1,3 @@
-import os
-import sys
-
-try:
-    import ipv8
-except ImportError:
-    # Make sure IPv8 can be imported
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-    sys.path.insert(0, os.path.join(dir_path, "..", "pyipv8"))
+"""
+This module contains code for Anydex.
+"""
