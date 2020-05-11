6b355009b3dd94bda6206a566a1512c8d3274917
Rahim Klabér <r.e.p.klaber@student.tudelft.nl>
2020-05-11 14:48:43 +0200

    created abstract provider

---
+++ anydex/wallet/provider.py
@@ -0,0 +1,40 @@
+import abc
+
+
+class Provider(metaclass=abc.ABCMeta):
+    """
+    Abstract class to create providers from.
+    A provider is an abstraction used to interact with the blockchain.
+
+    For each cryptocurrency, A new abstract class specific to that
+    cryptpcurrency should be created and any method specific to the cryptocurrency should be specified.
+    That class can than be subclassed and implemented.
+
+    """
+
+    @abc.abstractmethod
+    def submit_transaction(self, tx):
+        """
+        Submit a signed transaction to the network
+        :param tx: the signed transaction to submit to the network
+        :return: the transaction hash
+        """
+        return
+
+    @abc.abstractmethod
+    def get_balance(self, address):
+        """
+        Get the balance of the given address
+        :param address: address to get the balance of
+        :return: the balance
+        """
+        return
+
+    @abc.abstractmethod
+    def get_transactions(self, address):
+        """
+        Retrieve all the transactions associated with the given address
+        :param address: The address of which to retrieve the transactions
+        :return: A list of all transactions retrieved
+        """
+        return
