09ff464e3f87278d470f989b9349ffffec650142
Martijn de Vos <mdmartijndevos@gmail.com>
2021-10-22 15:01:18 +0200

    Converted TickEntry tests to pytest

--- anydex/test/core/test_tickentry.py
+++ anydex/test/core/test_tickentry.py
@@ -1,3 +1,5 @@
+import pytest
+
 from anydex.core.assetamount import AssetAmount
 from anydex.core.assetpair import AssetPair
 from anydex.core.message import TraderId
@@ -8,64 +10,69 @@ from anydex.core.tick import Tick
 from anydex.core.tickentry import TickEntry
 from anydex.core.timeout import Timeout
 from anydex.core.timestamp import Timestamp
-from anydex.test.base import AbstractServer
 
 
-class TickEntryTestSuite(AbstractServer):
-    """TickEntry test cases."""
+@pytest.fixture
+def price_level():
+    return PriceLevel(Price(100, 1, 'MB', 'BTC'))
 
-    async def setUp(self):
-        super(TickEntryTestSuite, self).setUp()
 
-        # Object creation
-        tick = Tick(OrderId(TraderId(b'0' * 20), OrderNumber(1)), AssetPair(AssetAmount(60, 'BTC'),
-                                                                            AssetAmount(30, 'MB')),
-                    Timeout(0), Timestamp(0), True)
-        tick2 = Tick(OrderId(TraderId(b'0' * 20), OrderNumber(2)),
-                     AssetPair(AssetAmount(63400, 'BTC'), AssetAmount(30, 'MB')), Timeout(100), Timestamp.now(), True)
+@pytest.fixture
+async def tick_entry(price_level):
+    tick = Tick(OrderId(TraderId(b'0' * 20), OrderNumber(1)),
+                AssetPair(AssetAmount(60, 'BTC'), AssetAmount(30, 'MB')), Timeout(0), Timestamp(0), True)
+    tick_entry = TickEntry(tick, price_level)
+    yield tick_entry
+    await tick_entry.shutdown_task_manager()
 
-        self.price_level = PriceLevel(Price(100, 1, 'MB', 'BTC'))
-        self.tick_entry = TickEntry(tick, self.price_level)
-        self.tick_entry2 = TickEntry(tick2, self.price_level)
 
-    async def tearDown(self):
-        await self.tick_entry.shutdown_task_manager()
-        await self.tick_entry2.shutdown_task_manager()
-        await super(TickEntryTestSuite, self).tearDown()
+@pytest.fixture
+async def tick_entry2(price_level):
+    tick = Tick(OrderId(TraderId(b'0' * 20), OrderNumber(2)),
+                AssetPair(AssetAmount(63400, 'BTC'), AssetAmount(30, 'MB')), Timeout(100), Timestamp.now(), True)
+    tick_entry = TickEntry(tick, price_level)
+    yield tick_entry
+    await tick_entry.shutdown_task_manager()
 
-    def test_price_level(self):
-        self.assertEqual(self.price_level, self.tick_entry.price_level())
 
-    def test_next_tick(self):
-        # Test for next tick
-        self.assertEqual(None, self.tick_entry.next_tick)
-        self.price_level.append_tick(self.tick_entry)
-        self.price_level.append_tick(self.tick_entry2)
-        self.assertEqual(self.tick_entry2, self.tick_entry.next_tick)
+def test_price_level(price_level, tick_entry):
+    assert price_level == tick_entry.price_level()
 
-    def test_prev_tick(self):
-        # Test for previous tick
-        self.assertEqual(None, self.tick_entry.prev_tick)
-        self.price_level.append_tick(self.tick_entry)
-        self.price_level.append_tick(self.tick_entry2)
-        self.assertEqual(self.tick_entry, self.tick_entry2.prev_tick)
 
-    def test_str(self):
-        # Test for tick string representation
-        self.assertEqual('60 BTC\t@\t0.5 MB', str(self.tick_entry))
+def test_next_tick(price_level, tick_entry, tick_entry2):
+    # Test for next tick
+    assert not tick_entry.next_tick
+    price_level.append_tick(tick_entry)
+    price_level.append_tick(tick_entry2)
+    assert tick_entry.next_tick == tick_entry2
 
-    def test_is_valid(self):
-        # Test for is valid
-        self.assertFalse(self.tick_entry.is_valid())
-        self.assertTrue(self.tick_entry2.is_valid())
 
-    def test_block_for_matching(self):
-        """
-        Test blocking of a match
-        """
-        self.tick_entry.block_for_matching(OrderId(TraderId(b'a' * 20), OrderNumber(3)))
-        self.assertEqual(len(self.tick_entry._blocked_for_matching), 1)
+def test_prev_tick(price_level, tick_entry, tick_entry2):
+    # Test for previous tick
+    assert not tick_entry.prev_tick
+    price_level.append_tick(tick_entry)
+    price_level.append_tick(tick_entry2)
+    assert tick_entry2.prev_tick == tick_entry
 
-        # Try to add it again - should be ignored
-        self.tick_entry.block_for_matching(OrderId(TraderId(b'a' * 20), OrderNumber(3)))
-        self.assertEqual(len(self.tick_entry._blocked_for_matching), 1)
+
+def test_str(tick_entry):
+    # Test for tick string representation
+    assert str(tick_entry) == '60 BTC\t@\t0.5 MB'
+
+
+def test_is_valid(tick_entry, tick_entry2):
+    # Test for is valid
+    assert not tick_entry.is_valid()
+    assert tick_entry2.is_valid()
+
+
+def test_block_for_matching(tick_entry):
+    """
+    Test blocking of a match
+    """
+    tick_entry.block_for_matching(OrderId(TraderId(b'a' * 20), OrderNumber(3)))
+    assert len(tick_entry._blocked_for_matching) == 1
+
+    # Try to add it again - should be ignored
+    tick_entry.block_for_matching(OrderId(TraderId(b'a' * 20), OrderNumber(3)))
+    assert len(tick_entry._blocked_for_matching) == 1
